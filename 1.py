ef93d2b6d2953cad88b15f4a3b90e7ca6f7c9448
Alexandre Boyer <biggnou@gmail.com>
2015-12-27 11:44:48 -0500

    First challenge

---
+++ 1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import hashlib
+
+mystring = raw_input("Give a string to hash:")
+
+def main():
+    rolo = rololo = ''
+    rolo = hashlib.sha256(mystring).hexdigest()
+    rololo = hashlib.md5(rololo).hexdigest()
+    print 'md5 hex digest of the sha256 hex digest of the \'', mystring, '\' string is: ', rololo
+
+if __name__ == '__main__':
+    main()
