801bcb0966628fe5989488899d8a14ebc20bf492
Alexandre Boyer <biggnou@gmail.com>
2015-12-27 22:55:33 -0500

    back to first chalenge.
    Do it in one pass instead of multiple variables.
    As seen in the discuss the solution :-)

--- 1.py
+++ 1.py
@@ -6,9 +6,10 @@ import hashlib
 mystring = raw_input("Give a string to hash:")
 
 def main():
-    rolo = rololo = ''
-    rolo = hashlib.sha256(mystring).hexdigest()
-    rololo = hashlib.md5(rolo).hexdigest()
+    # rolo = rololo = ''
+    # rolo = hashlib.sha256(mystring).hexdigest()
+    # rololo = hashlib.md5(rolo).hexdigest()
+    rololo = hashlib.md5(hashlib.sha256(mystring).hexdigest()).hexdigest()
     print 'md5 hex digest of the sha256 hex digest of the \'', mystring, '\' string is: ', rololo
 
 if __name__ == '__main__':
