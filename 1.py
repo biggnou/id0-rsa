5b800bc1794d353574eaff4e9a5ad4892b2cfb45
Alexandre Boyer <biggnou@gmail.com>
2015-12-27 22:47:17 -0500

    of course rolo...

--- 1.py
+++ 1.py
@@ -8,7 +8,7 @@ mystring = raw_input("Give a string to hash:")
 def main():
     rolo = rololo = ''
     rolo = hashlib.sha256(mystring).hexdigest()
-    rololo = hashlib.md5(rololo).hexdigest()
+    rololo = hashlib.md5(rolo).hexdigest()
     print 'md5 hex digest of the sha256 hex digest of the \'', mystring, '\' string is: ', rololo
 
 if __name__ == '__main__':
